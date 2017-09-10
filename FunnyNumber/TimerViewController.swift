03d0a6718f01ef959ac6fc18006a2343652c1581
pok1709 <pok1709@gmail.com>
2017-09-10 14:24:48 +0700

    Timer Done

--- FunnyNumber/TimerViewController.swift
+++ FunnyNumber/TimerViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
+    //Explicit, Implicit
+    var numberAint: Int = 1
+    
+    var objTimer = Timer()
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBAction func playButton(_ sender: Any) {
+        print("Click Play")
+        
+        objTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(TimerViewController.playTime), userInfo: nil, repeats: true)
+    }
+    
+    func playTime() -> Void {
+        numberAint += 1
+        numberLabel.text = String(numberAint)
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        print("Click Stop")
+        
+        objTimer.invalidate()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
