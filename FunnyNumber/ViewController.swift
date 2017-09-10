4710e52633d0bf04b730625f32fb2a256d216d6d
pok1709 <pok1709@gmail.com>
2017-09-10 09:29:18 +0700

    Initial Commit

---
+++ FunnyNumber/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FunnyNumber
+//
+//  Created by alccmac on 9/10/2560 BE.
+//  Copyright © 2560 SorapongPh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
